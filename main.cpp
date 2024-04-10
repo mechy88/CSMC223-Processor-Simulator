e713e9a6f26bbccc4fab4ed9381f1f8ca636f3d6
mechy88 <miguelandrerepatacodo@gmail.com>
2024-04-10 20:38:17 +0800

    Added: main.cpp for program entry

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    //sample test code, might need to implement file reading to load instructions to memory sorry jasper hahahaha
+    string command;
+
+    while(true){
+        getline(cin, command);
+        cout << command << endl;
+    }
+    return 0;
+}
