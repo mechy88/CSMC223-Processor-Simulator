5ef417523b26f07df8a926cc323ffdd412826393
mechy88 <miguelandrerepatacodo@gmail.com>
2024-04-11 16:11:06 +0800

    Added: MVP memory component and sample test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <list>
+#include "./pseudo-hardware/memory.cpp"
 
 using namespace std;
+
+/*
+TODO:
+- Implement Memory
+- Make print function to check memory contents
+*/
+
 
 int main(){
     //sample test code, might need to implement file reading to load instructions to memory sorry jasper hahahaha
     string command;
 
-    while(true){
-        getline(cin, command);
-        cout << command << endl;
-    }
+    // load instructions to memory
+
+    //don't stop the beating
+
+    // for testing
+    // while(true){
+    //     getline(cin, command);
+    //     cout << command << endl;
+    // }
+
+    //testing memory
+    Memory memory;
+    memory.mems1.push_back(InstructionCell("000", "1F", "943", "24A", "AAF"));
+    memory.mems1.push_back(InstructionCell("001", "20", "777", "345", "FAA"));
+    memory.mems2.push_back(DataCell("FBB", "12345678912"));
+
+    memory.printMemory();
     return 0;
 }
---
+++ pseudo-hardware/memory.cpp
@@ -0,0 +1,87 @@
+#include <string>
+#include <map>
+#include <list>
+#include <iostream>
+
+using namespace std;
+
+class Cell;
+class InstructionCell;
+class DataCell;
+
+class Cell{
+private:
+
+public:
+    string address;
+};
+
+class InstructionCell : public Cell{
+private:
+
+public:
+    string opcode;
+    string operand1;
+    string operand2;
+    string destination;
+
+    InstructionCell(){
+        address = "###";
+        opcode = "##";
+        operand1 = "###";
+        operand2 = "###";
+        destination = "###";
+    }
+
+    InstructionCell(string add, string op, string oper1, string oper2, string desti){
+        address = add;
+        opcode = op;
+        operand1 = oper1;
+        operand2 = oper2;
+        destination = desti;
+    }
+
+    string returnOutput(){
+        return (address + opcode + operand1 + operand2 + destination);
+    }
+};
+
+class DataCell : public Cell{
+private:
+    
+public:
+    string data;
+
+    DataCell(){
+        address = "###";
+        data = "###########";
+    }
+
+    DataCell(string add, string d){
+        address = add;
+        data = d;
+    }
+    
+    string returnOutput(){
+        return (address + data);
+    }
+
+};
+
+class Memory{
+private:
+    
+public:
+    list<InstructionCell> mems1; //memory locations 000 - 7FF
+    list<DataCell> mems2; //memory locations 800 - FFF
+
+    void printMemory(){
+        for(InstructionCell cell : mems1){
+            cout << cell.returnOutput() << endl;
+        }
+        for(DataCell cell : mems2){
+            cout << cell.returnOutput() << endl;
+        }
+        
+    }
+};
