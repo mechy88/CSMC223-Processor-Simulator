d4500aa53f48b06f7bf2f8fe67135d998a856d48
greenG3m <114197391+greenG3m@users.noreply.github.com>
2024-04-11 08:50:35 +0800

    Update main.cpp - added file stream commands

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 using namespace std;
 
 int main(){
     //sample test code, might need to implement file reading to load instructions to memory sorry jasper hahahaha
     string command;
+    ifstream fileIn; //input file manipulator
+    string fileName = "instructionSet.txt"; //must have a .txt file in the repo that contains instructions 
+
+    fileIn.open(fileName);
+    /* Code that stores instructions to memory (preferrably unordered map) */
+    fileIn.close();
 
     while(true){
         getline(cin, command);
